b4f8e8b85cf0233a24ffbad1ec797b259029f55d
drillbits <neji@drillbits.jp>
2016-04-07 18:04:10 +0900

    Serve index and static files

--- src/main.go
+++ src/main.go
@@ -1,14 +1,68 @@
 package app
 
 import (
-	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
+func fileExists(filePath string) bool {
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return false
+	}
+	if fileInfo.IsDir() {
+		return false
+	}
+	return true
+}
+
+func emitHTML(c context.Context, w http.ResponseWriter, r *http.Request, statusCode int, filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		http.Error(w, err.Error(), 503)
+		return
+	}
+
+	body, err := ioutil.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), 503)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Last-Modified", time.Now().Format(http.TimeFormat))
+	w.Header().Set("Content-Length", strconv.Itoa(len(body)))
+	w.WriteHeader(200)
+	w.Write(body)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+	ctx := appengine.NewContext(r)
+
+	basePath := "publish/ng2/"
+	filePath := basePath + r.URL.Path
+
+	if fileExists(filePath) {
+		// serve static file
+		if strings.HasSuffix(filePath, ".html") {
+			emitHTML(ctx, w, r, 200, filePath)
+		} else {
+			http.ServeFile(w, r, filePath)
+		}
+		return
+	}
+
+	// fallback to index
+	filePath = basePath + "index.html"
+	emitHTML(ctx, w, r, 200, filePath)
 }
